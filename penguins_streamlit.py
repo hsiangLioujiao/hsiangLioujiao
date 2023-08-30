1cb036506722a895232c7c0f33ddd42cc283d742
hsiangLioujiao <99463623+hsiangLioujiao@users.noreply.github.com>
2023-08-30 15:09:07 +0800

    Update penguins_streamlit.py
    增加允許的帳號及密碼

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -6,6 +6,17 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 
+# Everything is accessible via the st.secrets dict:
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
+
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
